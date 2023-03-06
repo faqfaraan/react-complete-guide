407554ed4b66adbc93eb79e00c4307e1d7097231
Faraan Qureshi <faqfaraan@gmail.com>
2023-03-05 22:13:07 -0500

    switching to arrow functions

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,6 @@
 import './Card.css'
 
-function Card(props) {
+const Card = (props) => {
     //this allows for child uses of this component's classes to take effect on the Card component where it is being used
     const classes = 'card ' + props.className;
     return <div className={classes}>{props.children}</div>
