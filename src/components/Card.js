f7cae02cb09052480df57e4d905b4d6e2853553d
Faraan Qureshi <faqfaraan@gmail.com>
2023-03-05 19:44:44 -0500

    Utilizing Composition aka children props

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import './Card.css'
+
+function Card(props) {
+    //this allows for child uses of this component's classes to take effect on the Card component where it is being used
+    const classes = 'card ' + props.className;
+    return <div className={classes}>{props.children}</div>
+}
+
+export default Card;
